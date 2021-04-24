c0ecf392831e8b640ca1c2a94ff03db292c30474
Jana <j.kampfmeier@live.de>
2021-04-24 20:58:13 +0200

    Added Password validation for Register

--- app/src/main/java/com/example/gruppe9_se2/user/RegisterFragment.java
+++ app/src/main/java/com/example/gruppe9_se2/user/RegisterFragment.java
@@ -13,6 +13,9 @@ import androidx.fragment.app.Fragment;
 import com.example.gruppe9_se2.R;
 import com.google.android.material.textfield.TextInputLayout;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegisterFragment extends Fragment {
 
     @Override
@@ -26,7 +29,7 @@ public class RegisterFragment extends Fragment {
         // retrieve argument of name if available
 
         Bundle args = getArguments();
-        if(args != null){
+        if (args != null) {
             String name = args.getString("name", "");
             EditText et = view.findViewById(R.id.et_name);
             et.setText(name);
@@ -43,22 +46,29 @@ public class RegisterFragment extends Fragment {
             TextInputLayout passwordLayout = view.findViewById(R.id.et_password_layout);
 
             // print error message if name or password empty
-            if(name.getText().toString().equals("")){
+            if (name.getText().toString().equals("")) {
                 nameLayout.setError("Enter a nickname");
-            }
-            else {
+            } else {
                 nameLayout.setError(null);
             }
-            if(password.getText().toString().equals("")){
+
+            // all Password Validations
+            String regexNoSpace = "^(?=\\S+$).+$"; /* contains no white space */
+            String regexLowerUppercase = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&\\-+=()]).{8,20}$";
+            /* contains at least one lower and upper case letter and at least 8 and at most 20 characters and at least one of these special characters and one number*/
+            // todo nice to have: error message icon, that doesn't show up over the password visible icon (eye)
+            String p = password.getText().toString();
+            if (password.getText().toString().equals("")) {
                 passwordLayout.setError("Enter a password");
-            }
-            else {
+            } else if (!Pattern.compile(regexNoSpace).matcher(password.getText().toString()).matches()) {
+                passwordLayout.setError("Enter a valid password, containing no white spaces");
+            } else if (!Pattern.compile(regexLowerUppercase).matcher(p).matches()) {
+                passwordLayout.setError("A valid password (8-20) needs at least one lowercase and uppercase letter, one special character and one number");
+            } else {
                 passwordLayout.setError(null);
             }
 
             // todo check username if already in use
-            // todo check password for form
-            // todo print error message if incorrect
             // todo add user to db and authenticate user and lead to home page
         });
 
