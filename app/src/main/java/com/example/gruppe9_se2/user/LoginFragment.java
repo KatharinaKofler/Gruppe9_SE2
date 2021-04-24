cfcab293e666affa6393cfb87f8ce8ee12a8adaf
Jana <j.kampfmeier@live.de>
2021-04-24 18:07:40 +0200

    Added Fragment Classes
    simply returning ther inflated layout

---
+++ app/src/main/java/com/example/gruppe9_se2/user/LoginFragment.java
@@ -0,0 +1,21 @@
+package com.example.gruppe9_se2.user;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.fragment.app.Fragment;
+import com.example.gruppe9_se2.R;
+import java.util.EventListener;
+
+public class LoginFragment extends Fragment implements EventListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
