f0534c58df23a27e25d850396fda26d380cdde0b
carinadeutschl <carinadeu@edu.aau.at>
2021-04-26 07:55:26 +0200

    Inital Commit add colors add lobby

---
+++ app/src/main/java/com/example/gruppe9_se2/user/Lobby.java
@@ -0,0 +1,8 @@
+package com.example.gruppe9_se2.user;
+
+public class Lobby {
+    //player is able to enter a lobby
+    //player is able to leave a lobby
+    //player is able to start game via lobby
+    //player is able to join lobby
+}
