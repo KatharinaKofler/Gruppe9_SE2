b41c36b4469eb4ae7cc8230070be525d30b7ebbb
Jana <j.kampfmeier@live.de>
2021-05-04 15:31:33 +0200

    updated gamestart activity

--- app/src/main/java/com/example/gruppe9_se2/logic/GameStart.java
+++ app/src/main/java/com/example/gruppe9_se2/logic/GameStart.java
@@ -1,4 +1,20 @@
 package com.example.gruppe9_se2.logic;
 
-public class GameStart {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gruppe9_se2.R;
+
+public class GameStart extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_gamestart);
+
+    }
 }
