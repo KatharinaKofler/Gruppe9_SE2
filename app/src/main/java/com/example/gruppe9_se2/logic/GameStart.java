37ed5ba854b430c2860bc9d573ed42434d5dbb9b
Jana <j.kampfmeier@live.de>
2021-05-10 17:26:10 +0200

    Websocket Connection works

--- app/src/main/java/com/example/gruppe9_se2/logic/GameStart.java
+++ app/src/main/java/com/example/gruppe9_se2/logic/GameStart.java
@@ -1,13 +1,21 @@
 package com.example.gruppe9_se2.logic;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gruppe9_se2.R;
+import com.example.gruppe9_se2.api.base.ApiManager;
+import com.example.gruppe9_se2.user.Lobby;
+import com.google.gson.JsonObject;
 
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import io.socket.client.IO;
 import io.socket.client.Socket;
@@ -20,24 +28,46 @@ public class GameStart extends AppCompatActivity {
 
     private Socket mSocket;
 
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_gamestart);
 
-        //Migrate LobbyID from LobbyOverviewFragment
+        // Get JWT from ApiManager
+        String jwt = ApiManager.getToken();
+        // Get lobby Id
         Bundle b = getIntent().getExtras();
         String lobbyID = b.getString("LobbyID");
 
-        //Open Web Socket
 
-        IO.Options options = IO.Options.builder()
-                .setExtraHeaders(singletonMap("x-lobby-id", singletonList(lobbyID)))
-                .build();
+        // Build Options with extra Headers
+        HashMap<String, List<String>> extraHeaders = new HashMap<>();
+        extraHeaders.put("x-jwt", singletonList(jwt));
+        extraHeaders.put("x-lobby-id", singletonList(lobbyID));
 
-        mSocket = IO.socket(URI.create("https://gruppe9-se2-backend.herokuapp.com/"), options);
+        IO.Options options = IO.Options.builder().setExtraHeaders(extraHeaders).build();
+
+        // Create Socket
+        URI uri = URI.create("https://gruppe9-se2-backend.herokuapp.com/");
+
+        mSocket = IO.socket(uri, options);
+
+        // check for successfull connection
+        mSocket.on("connect", args -> {
+            Log.i("myLogs", "Connection successfull");
+        });
+
+        // error if connect failed
+        mSocket.on("connect_failed", args -> {
+            Log.e("myLogs", "connect_failed");
+        });
+        
 
         mSocket.connect();
+
     }
+
+
 }
