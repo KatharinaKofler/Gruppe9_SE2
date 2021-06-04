967d7fbf28224cfa5c3a9505d1fb00aa0566b149
carinadeutschl <carinadeu@edu.aau.at>
2021-06-04 08:25:20 +0200

    Some layout changes

--- app/src/main/java/com/example/gruppe9_se2/logic/Game.java
+++ app/src/main/java/com/example/gruppe9_se2/logic/Game.java
@@ -1,7 +1,9 @@
 package com.example.gruppe9_se2.logic;
 
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -17,6 +19,14 @@ public class Game extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // show content activity game
         setContentView(R.layout.activity_game);
+
+        // hide status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         // create FragmentContainerViews to show each Fragment, like Spielmitte, Musterreihe, Wand, ...
 
