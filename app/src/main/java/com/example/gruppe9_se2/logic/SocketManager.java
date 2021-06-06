d6a78832834e79dd9fbbd1e6a8b15d17371a5308
Jana <j.kampfmeier@live.de>
2021-06-06 15:57:30 +0200

    update makeSocket force new socket
    don't use the cached socket, create new one

--- app/src/main/java/com/example/gruppe9_se2/logic/SocketManager.java
+++ app/src/main/java/com/example/gruppe9_se2/logic/SocketManager.java
@@ -15,32 +15,30 @@ public class SocketManager {
 
     private static Socket instance;
 
-    public static Socket makeSocket(String lobbyId){
-        if(instance == null){
+    public static Socket makeSocket(String lobbyId) {
 
-            // Get JWT from ApiManager
-            String jwt = ApiManager.getToken();
+        // Get JWT from ApiManager
+        String jwt = ApiManager.getToken();
 
-            // Build Options with extra Headers
-            HashMap<String, List<String>> extraHeaders = new HashMap<>();
-            extraHeaders.put("x-jwt", singletonList(jwt));
-            extraHeaders.put("x-lobby-id", singletonList(lobbyId));
+        // Build Options with extra Headers
+        HashMap<String, List<String>> extraHeaders = new HashMap<>();
+        extraHeaders.put("x-jwt", singletonList(jwt));
+        extraHeaders.put("x-lobby-id", singletonList(lobbyId));
 
-            IO.Options options = IO.Options.builder().setExtraHeaders(extraHeaders).build();
+        IO.Options options = IO.Options.builder().setForceNew(true).setExtraHeaders(extraHeaders).build();
 
-            // Create Socket
-            URI uri = URI.create("https://gruppe9-se2-backend.herokuapp.com/");
-            //URI uri = URI.create("http://localhost:8080/");
+        // Create Socket
+        URI uri = URI.create("https://gruppe9-se2-backend.herokuapp.com/");
+        //URI uri = URI.create("http://localhost:8080/");
 
-            instance = IO.socket(uri, options);
-        }
+        instance = IO.socket(uri, options);
+
         return instance;
     }
 
-    public static Socket getSocket(){
+    public static Socket getSocket() {
         return instance;
     }
-
 
 
 }
