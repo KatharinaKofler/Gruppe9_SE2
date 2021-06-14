d42da860ce367c661d8a0eec133a16dc4ea12429
carinadeutschl <carinadeu@edu.aau.at>
2021-06-14 10:56:25 +0200

    Show results in full screen

--- app/src/main/java/com/example/gruppe9_se2/game/EndGameActivity.java
+++ app/src/main/java/com/example/gruppe9_se2/game/EndGameActivity.java
@@ -2,9 +2,11 @@ package com.example.gruppe9_se2.game;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -21,6 +23,16 @@ public class EndGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_endgame);
+
+        // hide status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+
 
         Bundle b = getIntent().getExtras();
 
