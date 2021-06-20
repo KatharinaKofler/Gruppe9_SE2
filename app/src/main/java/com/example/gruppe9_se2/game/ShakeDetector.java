dd52a1ad14420f6e380eef39d259eb7bdcd07e7d
carinadeutschl <carinadeu@edu.aau.at>
2021-06-20 15:58:26 +0200

    Fix cheat method and ShakeDetector

--- app/src/main/java/com/example/gruppe9_se2/game/ShakeDetector.java
+++ app/src/main/java/com/example/gruppe9_se2/game/ShakeDetector.java
@@ -4,10 +4,11 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.util.Log;
 
 public class ShakeDetector implements SensorEventListener {
 
-    private static final float SHAKE_THRESHOLD_GRAVITY = 1.0000001f; // change when using physical device //2.7f;
+    private static final float SHAKE_THRESHOLD_GRAVITY = 1.1f; // change when using physical device //2.7f;
     private static final int SHAKE_GAP_TIME_MS = 50000;
 
     // The following are used for the shake detection
@@ -37,14 +38,14 @@ public class ShakeDetector implements SensorEventListener {
         // gForce will be close to 1 when there is no movement.
         double gForce = Math.sqrt(gX * gX + gY * gY + gZ * gZ);
 
-
         if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+            Log.i("Sensor change", "gForce: "+gForce);
+
             final long now = System.currentTimeMillis();
             // ignore shake events too close to each other (500ms)
             if (mShakeTimestamp + SHAKE_GAP_TIME_MS > now) {
                 return;
             }
-            System.out.println(gForce);
 
             mShakeTimestamp = now;
 
