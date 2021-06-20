ca0e77d87f87c11cd2e058c0ca5e28fb6560a10e
KatharinaKofler <katharinako@edu.aau.at>
2021-06-20 18:32:29 +0200

    update PlayersFregment

--- app/src/main/java/com/example/gruppe9_se2/game/PlayersFragment.java
+++ app/src/main/java/com/example/gruppe9_se2/game/PlayersFragment.java
@@ -2,7 +2,6 @@ package com.example.gruppe9_se2.game;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -21,7 +20,7 @@ import com.example.gruppe9_se2.api.users.UsersApi;
 import com.example.gruppe9_se2.api.users.UsersResponse;
 import com.example.gruppe9_se2.logic.GameStart;
 
-import org.json.JSONArray;
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -40,11 +39,9 @@ public class PlayersFragment extends Fragment {
     LinearLayout playerButtonLayout;
     private GameStart gameStart;
     private boolean isInit = false;
-    private boolean callMarkMe = false;
-    private boolean markMe;
-    private JSONObject currentplayerArgs;
-    private boolean [] caught = new boolean[4];
+    private boolean[] caught = new boolean[4];
 
+    public ObservableInteger playersTurn = new ObservableInteger();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,6 +50,7 @@ public class PlayersFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_players, container, false);
 
         playerButtonLayout = view.findViewById(R.id.playersLinearLayout);
+        playersTurn.setOnIntegerChangeListener(this::onIntegerChanged);
 
         return view;
     }
@@ -68,6 +66,10 @@ public class PlayersFragment extends Fragment {
                 playerList.remove(player);
             }
         }
+    }
+
+    public ArrayList<String[]> getPlayerList() {
+        return playerList;
     }
 
     public void addPlayer(int n) {
@@ -88,15 +90,19 @@ public class PlayersFragment extends Fragment {
                         String playerId = playerList.get(i - 1)[0];
                         playerButton.setTag(R.id.playerId, playerId);
                         playerButton.setTag(R.id.points, 0);
-                        playerButton.setOnClickListener(view -> gameStart.requestPlayerBoard(playerId));
+                        playerButton.setOnClickListener(view -> {
+                            Log.e("PlayerFragment", "Player " + playerId + " pressed");
+                            gameStart.requestPlayerBoard(playerId);
+                        });
                     }
                     playerButton.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
-                    playerButton.setTextColor(Color.parseColor("#000000"));
+                    if (i == playersTurn.get())
+                        playerButton.setTextColor(getResources().getColor(R.color.errorTextColor));
+                    else playerButton.setTextColor(getResources().getColor(R.color.primaryTextColor));
                     playerButton.setTypeface(playerButton.getTypeface(), Typeface.BOLD);
                     playerButtonLayout.addView(playerButton, i);
                 }
                 isInit = true;
-                if (callMarkMe) markMe(markMe);
             }
             String text = playerList.get(n)[1] + "\n0 Points";
             ((Button) playerButtonLayout.getChildAt(n + 1)).setText(text);
@@ -117,14 +123,15 @@ public class PlayersFragment extends Fragment {
             int finalI = i;
             call.enqueue(new Callback<UsersResponse>() {
                 @Override
-                public void onResponse(Call<UsersResponse> call, Response<UsersResponse> response) {
+                public void onResponse(@NotNull Call<UsersResponse> call, @NotNull Response<UsersResponse> response) {
                     Log.e("getUser", "onResponse successfull: " + response.code());
+                    assert response.body() != null;
                     player[1] = response.body().getUsername();
                     addPlayer(finalI);
                 }
 
                 @Override
-                public void onFailure(Call<UsersResponse> call, Throwable t) {
+                public void onFailure(@NotNull Call<UsersResponse> call, @NotNull Throwable t) {
                     Log.e("getUser", "onFailure");
                 }
             });
@@ -132,14 +139,14 @@ public class PlayersFragment extends Fragment {
     }
 
     public void updatePoints(String id, int points) {
-        gameStart.runOnUiThread(()-> {
+        gameStart.runOnUiThread(() -> {
             boolean foundId = false;
             for (int i = 1; i < playerButtonLayout.getChildCount(); i++) {
                 Button playerButton = (Button) playerButtonLayout.getChildAt(i);
                 if (playerButton.getTag(R.id.playerId).equals(id)) {
                     foundId = true;
                     playerButton.setTag(R.id.points, points);
-                    String text = playerList.get(i-1)[1] + "\n" + points + " Points";
+                    String text = playerList.get(i - 1)[1] + "\n" + points + " Points";
                     playerButton.setText(text);
                 }
             }
@@ -154,83 +161,107 @@ public class PlayersFragment extends Fragment {
 
     public void responsePlayerBoard(JSONObject playerBoard) {
 
+
+        // todo check if JSONObject has all the elements and everything is accessed correctly
+        String playerId = null;
         try {
-            // todo check if JSONObject has all the elements and everything is accessed correctly
-            String playerId = playerBoard.getString("playerId");
-
-            for (int i = 0; i < playerList.size(); i++) {
-                if (playerList.get(i)[0].equals(playerId)) {
-                    Bundle bundle = new Bundle();
-
-                    bundle.putString("id", playerList.get(i)[0]);
-                    bundle.putString("name", playerList.get(i)[1]);
-                    bundle.putInt("points", (Integer) playerButtonLayout.getChildAt(i).getTag(R.id.points));
-                    bundle.putBoolean("caught", caught[i]);
-
-                    JSONArray pattern = playerBoard.getJSONArray("pattern");
-                    int[] patternArray = new int[pattern.length()];
-                    for (int j = 0; j < pattern.length(); j++) {
-                        patternArray[j] = pattern.getInt(j);
-                    }
-                    bundle.putIntArray("pattern", patternArray);
-
-                    JSONArray wall = playerBoard.getJSONArray("wall");
-                    int[] wallArray = new int[wall.length()];
-                    for (int j = 0; j < wall.length(); j++) {
-                        wallArray[j] = wall.getInt(j);
-                    }
-                    bundle.putIntArray("wall", wallArray);
-
-                    // start PlayerPopup Intent
-                    Intent intent = new Intent(requireContext(), PlayerPopup.class);
-                    intent.putExtras(bundle);
-                    startActivity(intent);
-                }
-            }
+            playerId = playerBoard.getString("id");
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-    }
-
-    public void markMe(boolean markMe) {
-        if (!isInit) {
-            callMarkMe = true;
-            this.markMe = markMe;
-        } else {
-            gameStart.runOnUiThread(()->{
-                Button button = (Button) playerButtonLayout.getChildAt(0);
-                if (markMe) button.setTextColor(getResources().getColor(R.color.errorTextColor));
-                else button.setTextColor(getResources().getColor(R.color.primaryTextColor));
-            });
-        }
-    }
-
-    public void markCurrentPlayer(JSONObject args) {
-        try {
-            String currentPlayer = args.getString("id");
-            for (int i = 0; i < playerList.size(); i++) {
-                Button currentPlayerButton = (Button) playerButtonLayout.getChildAt(i+1);
-                if (playerList.get(i)[0].equals(currentPlayer)) {
-                    currentPlayerButton.setTextColor(getResources().getColor(R.color.errorTextColor));
-                    markMe(false);
-                } else {
-                    currentPlayerButton.setTextColor(getResources().getColor(R.color.primaryTextColor));
-                }
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-    }
-    public void playerCaught (String id){
         for (int i = 0; i < playerList.size(); i++) {
-            if(playerList.get(i)[0].equals(id)){
+            if (playerList.get(i)[0].equals(playerId)) {
+                Bundle bundle = new Bundle();
+
+                bundle.putString("id", playerList.get(i)[0]);
+                bundle.putString("name", playerList.get(i)[1]);
+                bundle.putInt("points", (Integer) playerButtonLayout.getChildAt(i+1).getTag(R.id.points));
+                bundle.putBoolean("caught", caught[i]);
+
+                int[] patternArray = null;
+                try {
+                    JSONObject pattern = playerBoard.getJSONObject("pattern");
+                    patternArray = new int[pattern.length()];
+                    for (int j = 0; j < pattern.length(); j++) {
+                        patternArray[j] = pattern.getInt("row" + (j + 1));
+                    }
+                } catch (JSONException ignored) {
+                }
+                bundle.putIntArray("pattern", patternArray);
+
+                int[] wallArray = null;
+                try {
+                    JSONObject wall = playerBoard.getJSONObject("wall");
+                    wallArray = new int[wall.length()];
+                    for (int j = 0; j < wall.length(); j++) {
+                        wallArray[j] = wall.getInt("row" + (j + 1));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                bundle.putIntArray("wall", wallArray);
+
+                // start PlayerPopup Intent
+                Intent intent = new Intent(requireContext(), PlayerPopup.class);
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        }
+
+
+    }
+
+    public void markCurrentPlayer(int player) {
+        for (int i = 0; i < playerList.size() + 1; i++) {
+            Button currentPlayerButton = (Button) playerButtonLayout.getChildAt(i);
+            if (i == player)
+                currentPlayerButton.setTextColor(getResources().getColor(R.color.errorTextColor));
+            else
+                currentPlayerButton.setTextColor(getResources().getColor(R.color.primaryTextColor));
+        }
+    }
+
+    public void playerCaught(String id) {
+        for (int i = 0; i < playerList.size(); i++) {
+            if (playerList.get(i)[0].equals(id)) {
                 caught[i] = true;
             }
+        }
+    }
 
+    private void onIntegerChanged(int player) {
+        if (isInit) markCurrentPlayer(player);
+    }
+
+
+    public interface OnIntegerChangeListener {
+        void onIntegerChanged(int newValue);
+    }
+
+    public static class ObservableInteger {
+        private OnIntegerChangeListener listener;
+
+        private int value;
+
+        public void setOnIntegerChangeListener(OnIntegerChangeListener listener) {
+            this.listener = listener;
         }
 
+        public int get() {
+            return value;
+        }
+
+        public void set(int value) {
+            Log.i("set Current Player", String.valueOf(value));
+            this.value = value;
+
+            if (listener != null) {
+                listener.onIntegerChanged(value);
+            }
+        }
     }
+
+
 }
 
